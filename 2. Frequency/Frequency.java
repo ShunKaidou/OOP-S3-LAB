99dc4273cb4ccdeee83b4cb4fbf58f64b2751177
NihadTk <81925773+ShunKaidou@users.noreply.github.com>
2022-04-25 00:23:14 +0530

    Add files via upload

---
+++ 2. Frequency/Frequency.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+
+public class Frequency{
+
+	public static void main(String[] args) throws IOException{
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter the string: ");
+		String str = scan.nextLine();
+		
+		System.out.print("\nEnter character: ");
+		char c = (char)System.in.read();
+		
+		
+		int count = 0;
+				
+		for (int i = 0 ; i < str.length() ; i++){
+			
+			if (c == str.charAt(i)) count++;}
+	
+	
+		System.out.println("\nFrequency: "+ count);
+	
+	
+	}	
+	
+}
