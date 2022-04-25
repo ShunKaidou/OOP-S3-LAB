84b333d628e7cb2d05c90fc0e212793a6e18f439
NihadTk <81925773+ShunKaidou@users.noreply.github.com>
2022-04-26 00:38:21 +0530

    Add files via upload

---
+++ 7. String Tokenizer/StrTokenizerDemo.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+ 
+class StrTokenizerDemo {
+
+    
+	public static void main(String args[]) {
+    
+		Scanner scan = new Scanner(System.in);
+    
+    
+	        int n , sum = 0;
+        
+    
+	        System.out.println("Enter Integers:");
+	        String str = scan.nextLine();
+        
+    
+	        StringTokenizer st = new StringTokenizer(str, " ");
+    
+
+	    	System.out.println("Entered Digits:");
+	        while (st.hasMoreTokens()){
+    
+			String temp = st.nextToken();
+    
+			n = Integer.parseInt(temp);
+    
+			System.out.println(n);
+    
+			sum = sum + n;
+		}
+	
+    
+		System.out.println("Sum of the Integers is: " + sum);
+    
+		scan.close();
+ 
+	}
+}
