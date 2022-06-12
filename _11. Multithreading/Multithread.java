8ef6b4c3f36a8a28abc9f4e77b307fb1c71083b3
NihadTk <81925773+ShunKaidou@users.noreply.github.com>
2022-06-12 20:57:12 +0530

    Add files via upload

---
+++ _11. Multithreading/Multithread.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+
+class X implements Runnable {
+    static int random;
+    Random rand = new Random();
+
+    public void run() {
+        random = rand.nextInt(30);				//Generates a random number between 0 and 30
+        System.out.println(random);
+    }
+}
+
+
+class Y implements Runnable {
+    public void run() {
+        if (X.random % 2 == 0)
+            System.out.println((int) Math.pow(X.random, 2) + "\n");		//Prints the random number ^2
+            																//if the random number is even
+    }
+}
+
+
+class Z implements Runnable {
+    public void run() {
+        if (X.random % 2 != 0)
+            System.out.println((int) Math.pow(X.random, 3) + "\n");		//Prints the random number ^3
+            																//if the random number is odd
+    }
+}
+
+
+
+class Multithread {
+    public static void main(String args[]) {
+        for (int i = 0; i < 10; i++) {
+            Thread objX = new Thread(new X());
+            Thread objY = new Thread(new Y());
+            Thread objZ = new Thread(new Z());
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+            objX.start();
+            objY.start();
+            objZ.start();
+        }
+    }
+}
