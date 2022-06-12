bdf4219e5970e50d5d62577326941447c5faccec
NihadTk <81925773+ShunKaidou@users.noreply.github.com>
2022-06-12 21:18:20 +0530

    Update FileCopy.java

--- _10. File Copy/FileCopy.java
+++ _10. File Copy/FileCopy.java
@@ -2,14 +2,20 @@ import java.io.*;
 
 
 public class FileCopy {
+	
 	public static void main(String[] args) {
+		
+		
 		try {
+			
 			FileWriter fw = new FileWriter("CopiedFile.txt");
 			FileReader fr = new FileReader("InputFile.txt");
+			
 			int i=0;
+			
 			while((i=fr.read())!=-1) {
 				char j=(char)i;
-					fw.write(j);
+				fw.write(j);
 			}
 			
 			fr.close();
@@ -19,9 +25,11 @@ public class FileCopy {
 		}catch(FileNotFoundException e ) {
 			System.out.println("Error: File NOT Found (add a file with name \"InputFile.txt\") ");
 		}
+		
 		catch(IOException e){
 			System.out.println("IOException Found");
 		}
+		
 	}
 }
 
